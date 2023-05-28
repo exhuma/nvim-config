14c99614fcc6c3b269744f72ab8adbd7dd339cc2
Michel Albert <michel@albert.lu>
2023-05-28 15:32:22 +0200

    Add code-quality settings

---
+++ behaviour/codequality.lua
@@ -0,0 +1,12 @@
+function run() 
+  vim.o.autoindent = true
+  vim.o.expandtab = true
+  vim.o.shiftround = true
+  vim.o.shiftwidth = 4
+  vim.o.tabstop = 4
+  vim.o.textwidth = 79
+end
+
+return {
+  run=run
+}
