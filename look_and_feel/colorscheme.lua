04bd1f4eb5ea063ad3443228cc35a39bbcc17cb0
Michel Albert <michel@albert.lu>
2023-05-28 15:31:18 +0200

    Lazily initialise the color-scheme

--- look_and_feel/colorscheme.lua
+++ look_and_feel/colorscheme.lua
@@ -1,6 +1,9 @@
-function run() 
-  require('packer').use('romainl/Apprentice')
+function config()
   vim.cmd [[colorscheme apprentice]]
+end
+
+function run() 
+  require('packer').use({'romainl/Apprentice', config=config})
 end
 
 return {
