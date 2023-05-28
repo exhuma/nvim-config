369903997693edefd20c3d1f99b74f12677b68c5
Michel Albert <michel@albert.lu>
2023-05-28 15:32:46 +0200

    Minor vim UI changes

---
+++ behaviour/ui.lua
@@ -0,0 +1,22 @@
+function run()
+  vim.o.wildignore=table.concat(
+    {
+      "*.lnk",
+      "*~",
+      "*.bak",
+      "*.pyc",
+      "*/.hg/*",
+      "*/.svn/*",
+      "*/env/*",
+      "*.egg-info/*",
+      "*/__pycache__/*",
+      "doc/_build/*",
+    },
+    ","
+  )
+  vim.o.wildmode = "longest:full,full"
+end
+
+return {
+  run=run
+}
